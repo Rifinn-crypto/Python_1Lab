cd7a3dec4160a14853e9dac3fea6bf5cf9f68b8a
Nikita Raven <esh2015@list.ru>
2022-10-23 22:39:55 +0400

    Working with columns in a file

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ import csv
 
 URL = " https://www.cbr-xml-daily.ru/archive/2022/10/22/daily_json.js"
 HEADER = ["Day", "Exchange rate"]
+write_to_file = "C:/Users/esh20/Desktop/dataset.csv"
+
+with open(write_to_file, "w") as file:
+    dw = csv.DictWriter(file, delimiter=',',
+                        fieldnames=HEADER)
+    dw.writeheader()
